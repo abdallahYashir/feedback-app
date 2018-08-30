0c1b7cd0df81a994ef7f4d05a8723f0fa1fe8f0d
Abdallah Yashir <abdallah.yashir@chb-technologies.ch>
2018-08-30 11:55:42 +0400

    Added home page in route + gitignore

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pages#home' # root path
+  get '*path' => redirect('/') # redirect to root if any other url is entered
 end
